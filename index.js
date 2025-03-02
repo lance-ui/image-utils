c37ec6a049abc68bee8e504acdba016924ffedc2
Lance <149887891+lance-ui@users.noreply.github.com>
2025-03-02 08:21:19 +0200

    update index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+import { main } from './utils.js';
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+app.get('/images', async (res, rej) => {
+  const { url, tool } = req.query;
+  if(!tool)
+   return res.status(400).json({ "error": "missing tool parameter" })
+  else if(!url)
+   return res.status(400).json({ "error": "missing url parameter" })
+  else:
+   try{
+     main({ url, tool })
+   }catch (e){
+     return res.status(500).json({ "error": "internal server error" })
+   }
+})
+
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`)
+})
